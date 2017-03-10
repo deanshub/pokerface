6509da564ee263a6f0d963dfa939c251bcbf3dda
Dean Shub <deanshub@gmail.com>
2017-03-10 11:07:09 +0200

    changing url for graphql

--- client/store/lokkaClient.js
+++ client/store/lokkaClient.js
@@ -2,7 +2,7 @@ import {Lokka} from 'lokka'
 import {Transport} from 'lokka-transport-http'
 
 const client = new Lokka({
-  transport: new Transport('http://localhost:9031/graphql'),
+  transport: new Transport('/graphql'),
 })
 
 export default client
