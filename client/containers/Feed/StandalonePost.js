5676257fc78553e4dc5be119d1e7bc20cb3754db
Dean Shub <deanshub@gmail.com>
2017-08-05 18:43:52 +0300

    loader in standalone post

--- client/containers/Feed/StandalonePost.js
+++ client/containers/Feed/StandalonePost.js
@@ -1,7 +1,7 @@
 // @flow
 import React, { Component, PropTypes } from 'react'
 import { observer, inject } from 'mobx-react'
-import { Header, Container, Icon } from 'semantic-ui-react'
+import { Header, Container, Icon, Loader } from 'semantic-ui-react'
 
 import Post from './Post'
 
@@ -32,38 +32,42 @@ export default class StandalonePost extends Component {
   }
 
   render() {
-    const { post } = this.state
+    const { post, loading } = this.state
 
     return (
       <Container text textAlign="center">
-        {post?
-          <Post
-              post={post}
-              standalone
-          />
+        {
+          loading?
+          <Loader active inline="centered">Loading</Loader>
           :
-          <div>
-            <a href="/login">
-              <Header
-                  color="red"
-                  size="huge"
-                  style={{marginTop:20}}
-                  textAlign="center"
-              >
-                  <Header.Content>Pokerface.io</Header.Content>
-              </Header>
-            </a>
-            <Header size="huge">Post have been removed or is private...</Header>
-            <Header size="bug">Login and try again</Header>
-            <a href="/login">
-              <Icon
-                  color="red"
-                  link
-                  name="home"
-                  size="massive"
-              />
-            </a>
-          </div>
+          post?
+            <Post
+                post={post}
+                standalone
+            />
+            :
+            <div>
+              <a href="/login">
+                <Header
+                    color="red"
+                    size="huge"
+                    style={{marginTop:20}}
+                    textAlign="center"
+                >
+                    <Header.Content>Pokerface.io</Header.Content>
+                </Header>
+              </a>
+              <Header size="huge">Post have been removed or is private...</Header>
+              <Header size="bug">Login and try again</Header>
+              <a href="/login">
+                <Icon
+                    color="red"
+                    link
+                    name="home"
+                    size="massive"
+                />
+              </a>
+            </div>
         }
       </Container>
     )
