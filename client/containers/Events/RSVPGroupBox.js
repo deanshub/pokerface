3adc19e8b75a4c15693779f38e3f308b458a91de
Dean Shub <deanshub@gmail.com>
2017-08-06 07:37:57 +0300

    avatar click route by routing store

--- client/containers/Events/RSVPGroupBox.js
+++ client/containers/Events/RSVPGroupBox.js
@@ -1,47 +1,62 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { Image, Popup } from 'semantic-ui-react'
 import classnames from 'classnames'
 import style from './style.css'
+import { observer, inject } from 'mobx-react'
 
-const RSVPGroupBox =({ group, groupName, hideWhenEmpty }) => {
-  if (hideWhenEmpty && group.length === 0) {
-    return null
+
+@inject('routing')
+@observer
+export default class RSVPGroupBox extends Component {
+  static propTypes = {
+    group: PropTypes.shape().isRequired,
+    groupName: PropTypes.string.isRequired,
+    hideWhenEmpty: PropTypes.bool,
+    routing: PropTypes.shape(),
   }
 
-  return (
-    <div className={classnames(style.rsvpGroup)}>
-      <div className={classnames(style.rsvpGroupHeader)}>{group.length} {groupName}</div>
-      <div>
-        {group.map(({ username, fullname, avatar }) => {
-          const href = `/profile/${username}`
-          return (
-            <div key={username}>
-              <Popup
-                  content={fullname}
-                  on="hover"
-                  trigger={
-                    <Image
-                        href={href}
-                        inline
-                        shape="circular"
-                        size="mini"
-                        spaced
-                        src={avatar}
-                    />
-                  }
-              />
-            </div>
-          )
-        })}
+  handleAvatarClick(e,href){
+    const { routing } = this.props
+
+    e.preventDefault()
+    routing.push(href)
+  }
+
+  render(){
+    const { group, groupName, hideWhenEmpty } = this.props
+
+    if (hideWhenEmpty && group.length === 0) {
+      return null
+    }
+
+    return (
+      <div className={classnames(style.rsvpGroup)}>
+        <div className={classnames(style.rsvpGroupHeader)}>{group.length} {groupName}</div>
+        <div>
+          {group.map(({ username, fullname, avatar }) => {
+            const href = `/profile/${username}`
+            return (
+              <div key={username}>
+                <Popup
+                    content={fullname}
+                    on="hover"
+                    trigger={
+                      <Image
+                          href={href}
+                          inline
+                          onClick={(e)=>{this.handleAvatarClick(e, href)}}
+                          shape="circular"
+                          size="mini"
+                          spaced
+                          src={avatar}
+                      />
+                    }
+                />
+              </div>
+            )
+          })}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
-RSVPGroupBox.propTypes = {
-  group: PropTypes.shape().isRequired,
-  groupName: PropTypes.string.isRequired,
-  hideWhenEmpty: PropTypes.bool,
-}
-
-export default RSVPGroupBox
