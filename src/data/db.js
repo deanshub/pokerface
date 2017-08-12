ec5a6c44a2ada763765744552fce80570aefe5b4
Dean Shub <deanshub@gmail.com>
2017-08-12 11:00:04 +0300

    moving to webpack in the server

--- src/data/db.js
+++ src/data/db.js
@@ -1,16 +1,45 @@
 import mongoose from 'mongoose'
-mongoose.Promise = global.Promise
+mongoose.Promise = Promise
 
 import Player from './dbModels/Player'
 import Post from './dbModels/Post'
 import Comment from './dbModels/Comment'
 import Game from './dbModels/Game'
-import generateFakeData from './faker/generator'
+// import generateFakeData from './faker/generator'
 
 const connString = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost/pokerface`
+// mongoose.set('debug', true)
+mongoose.connection.once('open', function() {
+  console.log('MongoDB event open')
+
+  mongoose.connection.on('connected', function() {
+    console.log('MongoDB event connected')
+  })
+
+  mongoose.connection.on('disconnected', function() {
+    console.warn('MongoDB event disconnected')
+  })
+
+  mongoose.connection.on('reconnected', function() {
+    console.log('MongoDB event reconnected')
+  })
+
+  mongoose.connection.on('error', function(err) {
+    console.error(`MongoDB event error:${err}`)
+  })
+})
+
+  // return resolve();
+  // return server.start();
+
 mongoose.connect(connString,{
   useMongoClient: true,
-})
+  // socketTimeoutMS: 0,
+  // keepAlive: true,
+  // reconnectTries: 30,
+}).then(()=>{
+  console.log('Connected to DB successfuly')
+}).catch(console.error)
 
 const DB = {
   models:{
--- src/routes/staticRoutes.js
+++ src/routes/staticRoutes.js
@@ -1,5 +1,7 @@
 import path from 'path'
 import express from 'express'
+// import fs from 'fs'
+// import ReactDOMServer from 'react-dom/server'
 
 const STATIC_FILES_DIRECTORY = path.join(__dirname,'../../../client/static')
 const INDEX_HTML_PATH = path.join(STATIC_FILES_DIRECTORY, 'index.html')
@@ -24,13 +26,24 @@ router.use('/profile/:username', isAuthenticated,express.static(INDEX_HTML_PATH)
 router.use('/events', isAuthenticated,express.static(INDEX_HTML_PATH))
 router.use('/login', express.static(INDEX_HTML_PATH))
 router.use('/post/:id', express.static(INDEX_HTML_PATH))
+// router.get('/post/:id', function (req, res) {
+//   // and drop 'public' in the middle of here
+//   res.sendFile(path.join(STATIC_FILES_DIRECTORY, 'index.html'))
+//   fs.readFile('./index.html', 'utf8', function (err, file) {
+//     if (err) {
+//       return console.error(err)
+//     }
+//     const document = file.replace(/<div id="app"><\/div>/, `<div id="app">${html}</div>`)
+//     res.send(document)
+//   })
+// })
 // router.use('/', isAuthenticated, express.static(INDEX_HTML_PATH))
 
 
 router.use('/', express.static(STATIC_FILES_DIRECTORY))
 
 
-// app.get('*', function (req, res) {
+// router.get('*', function (req, res) {
 //   // and drop 'public' in the middle of here
 //   res.sendFile(path.join(STATIC_FILES_DIRECTORY, 'index.html'))
 // })
