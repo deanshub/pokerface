582511d9530be628c612dabe640f7bfad3d059d2
Dean Shub <deanshub@gmail.com>
2017-08-12 20:39:51 +0300

    fb url now dynamic by server side

--- src/routes/staticRoutes.js
+++ src/routes/staticRoutes.js
@@ -1,10 +1,11 @@
 import path from 'path'
 import express from 'express'
-// import fs from 'fs'
+import fs from 'fs'
 // import ReactDOMServer from 'react-dom/server'
 
 const STATIC_FILES_DIRECTORY = path.join(__dirname,'../../../client/static')
 const INDEX_HTML_PATH = path.join(STATIC_FILES_DIRECTORY, 'index.html')
+const metaFbUrl = '<meta property="og:url" content="http://pokerface.io"/>'
 
 function isAuthenticated(req, res, next) {
   if (req.isAuthenticated()){
@@ -25,18 +26,19 @@ router.use('/smart', isAuthenticated, express.static(INDEX_HTML_PATH))
 router.use('/profile/:username', isAuthenticated,express.static(INDEX_HTML_PATH))
 router.use('/events', isAuthenticated,express.static(INDEX_HTML_PATH))
 router.use('/login', express.static(INDEX_HTML_PATH))
-router.use('/post/:id', express.static(INDEX_HTML_PATH))
-// router.get('/post/:id', function (req, res) {
+// router.use('/post/:id', express.static(INDEX_HTML_PATH))
+router.get('/post/:id', function (req, res) {
 //   // and drop 'public' in the middle of here
 //   res.sendFile(path.join(STATIC_FILES_DIRECTORY, 'index.html'))
-//   fs.readFile('./index.html', 'utf8', function (err, file) {
-//     if (err) {
-//       return console.error(err)
-//     }
-//     const document = file.replace(/<div id="app"><\/div>/, `<div id="app">${html}</div>`)
-//     res.send(document)
-//   })
-// })
+  const postUrl = `${req.protocol}://${req.get('Host')}${req.url}`
+  fs.readFile(INDEX_HTML_PATH, 'utf8', function (err, file) {
+    if (err) {
+      return console.error(err)
+    }
+    const document = file.replace(metaFbUrl, `<meta property="og:url" content="${postUrl}"/>`)
+    res.send(document)
+  })
+})
 // router.use('/', isAuthenticated, express.static(INDEX_HTML_PATH))
 
 
