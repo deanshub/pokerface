871ec42c4921be06235606de73fc4cb19239db96
Dean Shub <deanshub@gmail.com>
2017-08-12 20:47:54 +0300

    adding a title for posts shared on facebook

--- src/routes/staticRoutes.js
+++ src/routes/staticRoutes.js
@@ -6,6 +6,9 @@ import fs from 'fs'
 const STATIC_FILES_DIRECTORY = path.join(__dirname,'../../../client/static')
 const INDEX_HTML_PATH = path.join(STATIC_FILES_DIRECTORY, 'index.html')
 const metaFbUrl = '<meta property="og:url" content="http://pokerface.io"/>'
+const metaFbTitle = '<meta property="og:title" content="Welcome to Pokerface.io"/>'
+// const metaFbDescription = '<meta property="og:description" content="Social platform for poker players."/>'
+
 
 function isAuthenticated(req, res, next) {
   if (req.isAuthenticated()){
@@ -36,6 +39,8 @@ router.get('/post/:id', function (req, res) {
       return console.error(err)
     }
     const document = file.replace(metaFbUrl, `<meta property="og:url" content="${postUrl}"/>`)
+      .replace(metaFbTitle, '<meta property="og:title" content="New post shared in Pokerface.io"/>')
+      // .replace(metaFbDescription, '<meta property="og:description" content="Social platform for poker players."/>')
     res.send(document)
   })
 })
