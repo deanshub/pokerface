e4f457e3e516c5aaf2d2348a0f0d937b866f2adf
Dean Shub <deanshub@gmail.com>
2017-11-14 07:44:43 +0200

    changing card back to show up on gif

--- client/components/Deck/Card.js
+++ client/components/Deck/Card.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import classnames from 'classnames'
 import style from './style.css'
 import {SUITES, normalizeSuite, normalizeRank} from './consts'
-
+import redLogo from '../../assets/pokerface_red.png'
 //TODO: take care of joker card
 //TODO: take care of clickable cards
 
@@ -19,9 +19,20 @@ export default class Card extends Component {
   static defaultProps = {
     active: false,
     covered: false,
-    coveredText: 'Pokerface.io',
+    coveredText: 'Poker face',
     noHoverEffect: false,
     size: 3.5,
+    coveredLogo: redLogo,
+  }
+
+  getBackCard(){
+    const {coveredText, coveredLogo} = this.props
+    return (
+      <div className={classnames(style.backContainer)}>
+        <div className={classnames(style.backLogo)} style={{backgroundImage:`url(${coveredLogo})`}}/>
+        <div className={classnames(style.backText)}>{coveredText}</div>
+      </div>
+    )
   }
 
   render() {
@@ -46,12 +57,12 @@ export default class Card extends Component {
           style={{
             width: `${size}vw`,
             height: `${size*1.4}vw`,
-            fontSize: `${size/(covered?4.5:1.3)}vw`,
+            fontSize: `${size/(covered?4.4:1.3)}vw`,
           }}
           {...letterAttr}
       >
           {covered?
-            coveredText
+            this.getBackCard()
             :
             SUITES[normalizedSuit]
           }
