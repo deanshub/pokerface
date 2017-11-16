bc9152174ec675311f906d5c38f39b5f7d1bbb34
Dean Shub <deanshub@gmail.com>
2017-11-16 09:06:59 +0200

    restyling the poker table

--- client/components/PokerTable/index.js
+++ client/components/PokerTable/index.js
@@ -4,26 +4,31 @@ import style from './style.css'
 
 export default class PokerTable extends Component {
   static propTypes = {
+    edgeColor: PropTypes.string,
+    fabricColor: PropTypes.string,
     standalone: PropTypes.bool,
     title: PropTypes.string,
   }
   static defaultProps = {
+    fabricColor: '#388E3C',
+    edgeColor: '#353535',
     title: 'Pokerface.io',
     standalone: false,
   }
 
   render() {
-    const {children, title, standalone} = this.props
+    const {children, title, standalone, fabricColor, edgeColor} = this.props
     return (
       <div
           className={classnames(
             style.table,
             {[style.standalone]:standalone}
           )}
+          style={{backgroundColor: edgeColor}}
       >
         {children}
-        <div className={classnames(style.edge)}>
-          <div className={classnames(style.fabric)}>
+        <div className={classnames(style.edge)} style={{backgroundColor: fabricColor}}>
+          <div className={classnames(style.fabric)} style={{backgroundColor: fabricColor}}>
             <div className={classnames(style.text)}>
               {title}
             </div>
