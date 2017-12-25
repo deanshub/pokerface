28af4c037b66528a6d34d90e9517d3c19b08fe51
shamit6 <amishtalev@gmail.com>
2017-12-25 18:09:09 +0200

    with no organizations in players

--- client/store/AuthStore.js
+++ client/store/AuthStore.js
@@ -5,20 +5,25 @@ import request from 'superagent'
 import { close } from './graphqlClient'
 import logger from '../utils/logger'
 import {deleteCookie, getCookieByName} from '../utils/cookies'
+import {optionalUsersSwitchQuery, optionalUsersLoginQuery} from './queries/users'
+import graphqlClient from './graphqlClient'
 
 export class AuthStore {
   @observable token
   @observable user
+  @observable optionalUsers
   @observable opensourceModalOpen: boolean
   @observable modalModalOpen: boolean
   @observable authenticating: boolean
+  @observable fetchOptionalUsers: boolean
 
   constructor(){
     this.user = {}
     this.authenticating = true
-    // this.authenticate()
+    this.fetchOptionalUsers = true
     this.opensourceModalOpen = false
     this.modalModalOpen = false
+    this.optionalUsers = observable([])
   }
 
   @action
@@ -31,7 +36,6 @@ export class AuthStore {
 
   @action
   authenticate(){
-
     this.authenticating = true
     return request.post('/login/isAuthenticated').set('Authorization', localStorage.getItem('jwt')).then((res)=>{
       this.authenticating = false
@@ -40,11 +44,7 @@ export class AuthStore {
 
       logger.setField({user:user.username, email:user.email})
 
-      const cookieJwt = getCookieByName('jwt')
-      if (cookieJwt !== null){
-        localStorage.setItem('jwt',cookieJwt)
-        deleteCookie('jwt')
-      }
+      this.moveCookieJwtToLocalStorage()
 
       return this.user=user
     }).catch(err=>{
@@ -54,9 +54,11 @@ export class AuthStore {
   }
 
   @action
-  switchToOrganization(organizationId){
+  switchToUser(userId){
+    this.moveCookieJwtToLocalStorage()
+
     this.authenticating = true
-    return request.post('/login/switchToOrganization').send({organizationId}).set('Authorization', localStorage.getItem('jwt')).then((res)=>{
+    return request.post('/login/switchToUser').send({userId}).set('Authorization', localStorage.getItem('jwt')).then((res)=>{
       this.authenticating = false
 
       const {token} = res.body
@@ -76,6 +78,39 @@ export class AuthStore {
 
   @action
   refresh(){
+    this.authenticating = true
+    this.fetchOptionalUsers = true
+    this.optionalUsers = observable([])
     return this.authenticate()
+  }
+
+  @action
+  fetchOptionalUsersSwitch(){
+    this.fetchOptionalUsers = true
+    return graphqlClient.query({query:optionalUsersSwitchQuery, fetchPolicy:'network-only'}).then((result) =>{
+      this.optionalUsers = result.data.optionalUsersSwitch
+      this.fetchOptionalUsers = false
+      return this.optionalUsers
+    })
+  }
+
+  @action
+  fetchOptionalUsersLogin(){
+    this.fetchOptionalUsers = true
+    return graphqlClient.query({query:optionalUsersLoginQuery, fetchPolicy:'network-only'}).then((result) =>{
+      this.optionalUsers = result.data.optionalUsersLogin
+      this.fetchOptionalUsers = false
+      return this.optionalUsers
+    })
+  }
+
+  // When login from facebook and google plus the jwt is put in the cookies
+  // and we need to move it to local store
+  moveCookieJwtToLocalStorage(){
+    const cookieJwt = getCookieByName('jwt')
+    if (cookieJwt !== null){
+      localStorage.setItem('jwt',cookieJwt)
+      deleteCookie('jwt')
+    }
   }
 }
