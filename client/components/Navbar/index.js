017a124750b80951e3a9d5a817f3bc8ef4996281
shamit6 <amishtalev@gmail.com>
2017-12-30 16:55:03 +0200

    before blur

--- client/components/Navbar/index.js
+++ client/components/Navbar/index.js
@@ -7,7 +7,7 @@ import { observer, inject } from 'mobx-react'
 import PlayerSearchResult from './PlayerSearchResult'
 import classnames from 'classnames'
 import style from './style.css'
-import SelectUserModal from '../../containers/SelectUserModal'
+import { NavLink } from 'react-router-dom'
 
 @inject('globalPlayersSearch')
 @inject('routing')
@@ -69,85 +69,56 @@ export default class Navbar extends Component {
     const showSwitchUser = auth.optionalUsers.length > 0
 
     return (
-        <Menu
-            fixed="top"
-            pointing
-            secondary
-            size="large"
-            style={{marginBottom:0, backgroundColor:'white'}}
-        >
-          <Menu.Item
-              className={classnames(style.navbarMenuItemAnchor)}
-              active={this.isActive('/', true)}
-              onClick={()=>this.handleMenuItemClick('/')}
+      <div className={classnames(style.navbar)}>
+          <NavLink
+              activeClassName={classnames(style.navbarRouteItemActive)}
+              className={classnames(style.navbarRouteItem)}
+              exact
+              to="/"
           >
-            <Icon name="home"/> Home
-          </Menu.Item>
-          <Menu.Item
-              className={classnames(style.navbarMenuItemAnchor)}
-              active={this.isActive('/profile', true)}
-              onClick={()=>this.handleMenuItemClick('/profile')}
+            HOME
+          </NavLink>
+          <NavLink
+              activeClassName={classnames(style.navbarRouteItemActive)}
+              className={classnames(style.navbarRouteItem)}
+              exact
+              to="/profile"
           >
-            {auth.user.avatar?
-              <Image
-                  avatar
-                  size="mini"
-                  src={auth.user.avatar}
-                  style={{marginTop:-5,marginBottom:-10, marginRight:10, maxHeight:35}}
-              />
-              :
-              <Icon name="user"/>
-            } Profile
-          </Menu.Item>
-          <Menu.Item
-              active={this.isActive('/events', true)}
-              className={classnames(style.navbarMenuItemAnchor)}
-              onClick={()=>this.handleMenuItemClick('/events')}
+            PROFILE
+          </NavLink>
+          <NavLink
+              activeClassName={classnames(style.navbarRouteItemActive)}
+              className={classnames(style.navbarRouteItem)}
+              exact
+              to="/events"
           >
-            <Icon name="calendar"/> Events
+            EVENTS
             {
               events.games.size>0?
               <Label circular size="mini">{events.games.size}</Label>:
               undefined
             }
-          </Menu.Item>
-
-
-          <Menu.Menu position="right">
-            <Menu.Item
-                active={this.isActive('/smart', true)}
-                className={classnames(style.navbarMenuItemAnchor)}
-                onClick={()=>this.handleMenuItemClick('/smart')}
+          </NavLink>
+          <div className={classnames(style.navbarSection)}>
+            TOOLS
+          </div>
+            <NavLink
+                activeClassName={classnames(style.navbarRouteItemActive)}
+                className={classnames(style.navbarRouteItem)}
+                exact
+                to="/smart"
             >
-              <Icon name="student"/> Get Smarter
-            </Menu.Item>
-            <Menu.Item
-                active={this.isActive('/timer', true)}
-                className={classnames(style.navbarMenuItemAnchor)}
-                onClick={()=>this.handleMenuItemClick('/timer')}
+              PER-FLOP CHART
+            </NavLink>
+            <NavLink
+                activeClassName={classnames(style.navbarRouteItemActive)}
+                className={classnames(style.navbarRouteItem)}
+                exact
+                to="/timer"
             >
-              <Icon name="clock"/> Blinds Timer
-            </Menu.Item>
-            <Dropdown
-                className="link item"
-                icon={<Icon name="user" size="large"/>}
-            >
-              <Dropdown.Menu style={{minWidth:'max-content'}}>
-                {
-                  showSwitchUser &&
-                  <Dropdown.Item onClick={() => this.setState({selectUserModalOpen:true})}>
-                      switch user
-                  </Dropdown.Item>
-                }
-                <Dropdown.Item
-                    className={classnames(style.navbarMenuItemAnchor)}
-                    onClick={::this.handleLogout}
-                >
-                  logout
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-            <Menu.Item>
+              BLINDS TIMER
+            </NavLink>
+            <div>
               <Search
                   as={Input}
                   icon={{ name: 'search', link: true }}
@@ -162,17 +133,8 @@ export default class Navbar extends Component {
                   transparent
                   value={globalPlayersSearch.searchValue}
               />
-            </Menu.Item>
-          </Menu.Menu>
-          {
-            selectUserModalOpen &&
-            <SelectUserModal
-                onClose={() => this.setState({selectUserModalOpen:false})}
-                open={selectUserModalOpen}
-                redirectUrl="/"
-            />
-          }
-        </Menu>
+            </div>
+          </div>
     )
   }
 }
--- client/containers/Navigation/index.js
+++ client/containers/Navigation/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import DocumentTitle from 'react-document-title'
 import DevTools from 'mobx-react-devtools'
 import { Route, Switch } from 'react-router-dom'
+import { observer, inject } from 'mobx-react'
 
 import Navbar from '../../components/Navbar'
 import Feed from '../Feed'
@@ -12,31 +13,78 @@ import Events from '../Events'
 import RebrandedBlindsTimer from '../RebrandedBlindsTimer'
 import Learn from '../../components/Learn'
 import NoMatch from '../../components/NoMatch'
+import SelectUserModal from '../../containers/SelectUserModal'
 
 import 'semantic-ui-css/semantic.min.css'
 import classnames from 'classnames'
 import style from './style.css'
 
+@inject('auth')
+@inject('routing')
+@observer
 export default class Navigation extends Component {
   static propTypes={
     children: PropTypes.element,
   }
 
+  state = {showTopMenu:false, selectUserModalOpen:false}
+
+  handleLogout(){
+    const {routing, auth} = this.props
+    localStorage.removeItem('jwt')
+    auth.logout()
+    routing.replace('/login')
+  }
+
   render() {
+
+    const {user} = this.props.auth
+    const {showTopMenu, selectUserModalOpen} = this.state
+
+    const topMenu = <div className={classnames(style.topMenu)}>
+      <div className={classnames(style.signedInAsCard)}>
+        <img src={user.avatar}/>
+        <div>
+          <div className={classnames(style.signedInAs)}>Signed is as</div>
+          <div>{user.fullname}</div>
+        </div>
+      </div>
+      <div
+          className={classnames(style.topMenuItem)}
+          onClick={() => this.setState({selectUserModalOpen:true})}
+      >
+        Switch user
+      </div>
+      <div className={classnames(style.topMenuItem)} onClick={::this.handleLogout}>Logout</div>
+    </div>
+
     return (
       <DocumentTitle title="Pokerface.io">
         <div>
-          {
+          {/* {
             process.env.NODE_ENV==='development'?<DevTools/>:null
-          }
-          <Navbar />
-          <div className={classnames(style.container)} style={{paddingTop: 50}}>
-            <Switch>
-              <Route
-                  component={Feed}
-                  exact
-                  path="/"
-              />
+          } */}
+          <div className={classnames(style.header)}>
+            <div className={classnames(style.title)}>
+              POKERFACE
+            </div>
+            <div
+                className={classnames(style.config)}
+                onBlur={() => {this.setState({showTopMenu:false})}}
+                onClick={() => {this.setState({showTopMenu:true})}}
+            >
+              {showTopMenu && topMenu}
+            </div>
+          </div>
+          <div className={classnames(style.container)}>
+            <Navbar />
+            <div>
+              <Switch>
+                <Route
+                    component={Feed}
+                    exact
+                    path="/"
+                />
 
               <Route
                   component={Profile}
@@ -65,6 +113,15 @@ export default class Navigation extends Component {
               />
               <Route component={NoMatch}/>
             </Switch>
+          </div>
+          {
+            selectUserModalOpen &&
+            <SelectUserModal
+                onClose={() => this.setState({selectUserModalOpen:false})}
+                open={selectUserModalOpen}
+                redirectUrl="/"
+            />
+          }
           </div>
         </div>
       </DocumentTitle>
