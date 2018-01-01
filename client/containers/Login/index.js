0da1eb3155187f1838d2e6768185560862059921
Dean Shub <deanshub@gmail.com>
2018-01-01 10:27:05 +0200

    initial commit of landing page redesign

--- client/containers/Login/index.js
+++ client/containers/Login/index.js
@@ -2,23 +2,36 @@
 
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
-// import { observer, inject } from 'mobx-react'
-// import { Grid, Header, Form, Segment, Button, Icon, Divider, Message } from 'semantic-ui-react'
-import { Segment } from 'semantic-ui-react'
 // import request from 'superagent'
-// import R from 'ramda'
 // import logger from '../../utils/logger'
-import PublicPageTemplate from '../../components/PublicPageTemplate'
-import SignupForm from './SignupForm'
-import LoginForm from './LoginForm'
+import classnames from 'classnames'
+import style from './style.css'
 
-export default () => (
-  <PublicPageTemplate horizontal>
-      <Segment basic>
-        <LoginForm/>
-      </Segment>
-      <Segment basic>
-        <SignupForm/>
-      </Segment>
-  </PublicPageTemplate>
-)
+// TODO: remove loginForm and signUpForm components when done
+export default class Login extends Component {
+  render(){
+    return(
+      <div className={classnames(style.landing)}>
+        <div className={classnames(style.sectionOne)}>
+          <div className={classnames(style.header)}>
+            <div className={classnames(style.title)}>
+              Pokerface.io
+            </div>
+            <div className={classnames(style.login)}>
+              Already have an account?
+            </div>
+          </div>
+
+          <div className={classnames(style.mainContent)}>
+            <div className={classnames(style.leftMain)}>
+              pokerface
+            </div>
+            <div className={classnames(style.rightMain)}>
+              login
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
