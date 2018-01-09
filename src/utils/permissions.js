35968e8e4a8e313db2c11636e2352f306bdeb3a1
shamit6 <amishtalev@gmail.com>
2018-01-09 20:09:45 +0200

    CREATE_PUBLIC_EVENT

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -1,6 +1,7 @@
 // organization
 export const REBRANDING = 'rebranding'
 export const LOGIN = 'login'
+export const CREATE_PUBLIC_EVENT = 'createPublicEvent'
 
 // event
 export const PUBLIC = 'public'
